a7c24644cf6ea6bab368788216e9757c1af4f5e0
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-01-26 17:41:59 -0300

    Add isometric movement perspective to player

---
+++ survive-the-night/Assets/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement; //stores x and y
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Control input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    // work same as Update but it control physics
+    private void FixedUpdate()
+    {
+        //Control movement
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
