a386d00d9fc87eb54a7ab5631f86bf23f8065ee2
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-01-26 21:19:39 -0300

    Add animation to attack right

---
+++ survive-the-night/Assets/PlayerCombat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+        
+    }
+
+    void Attack()
+    {
+        //Playe an attack animation
+        animator.SetTrigger("Attack");
+        //Detect enemies in range of attack
+        //Damage them
+
+    }
+}
