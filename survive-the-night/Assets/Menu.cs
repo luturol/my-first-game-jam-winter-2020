443e64e7e3a61518732a293da723a14161cefb08
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-04-19 02:03:39 -0300

    Add event to start button

---
+++ survive-the-night/Assets/Menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{    
+    public string GameScene;
+    public string AboutScene;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(GameScene);
+    }
+
+    public void ShowAbout()
+    {
+        SceneManager.LoadScene(AboutScene);
+    }    
+}
