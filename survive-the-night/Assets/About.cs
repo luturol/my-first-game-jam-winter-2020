8631f292652a25f00e9a5f8a99f7bb0ce73787b6
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-04-19 02:27:37 -0300

    Added about scene

---
+++ survive-the-night/Assets/About.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class About : MonoBehaviour
+{
+    public string MenuScene;
+    
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(MenuScene);
+    }    
+}
