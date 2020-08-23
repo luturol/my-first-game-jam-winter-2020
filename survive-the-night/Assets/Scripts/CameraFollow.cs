318ee5c8df16c4f613b6db08440d9a9c2543e01b
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-08-23 11:44:50 -0300

    Add player and camera following him

---
+++ survive-the-night/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform playerTransform;
+    
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectsWithTag("Player")[0].transform;        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 cameraTemp = transform.position;
+        cameraTemp.x = playerTransform.position.x;
+
+        if(playerTransform.position.y > 0 || playerTransform.position.y < 0)
+        {
+            cameraTemp.y = playerTransform.position.y;
+        }
+        
+        transform.position = cameraTemp;
+    }
+}
